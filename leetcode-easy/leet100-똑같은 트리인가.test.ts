0339272c261ce277444fc6e963ec4ec0effb7c03
DevOcean Han <soa6318@gmail.com>
2023-08-28 16:28:11 +0900

    [수정][Leet100] 테스트 코드 덧붙여 완성 및 TreeNode클래스에 printTree() 메소드 추가

--- leetcode-easy/leet100-똑같은 트리인가.test.ts
+++ leetcode-easy/leet100-똑같은 트리인가.test.ts
@@ -3,10 +3,132 @@ const { solution, TreeNode } = source;
 
 describe(`Same Tree`, () => {
 	describe('The number of nodes check: is in the range [0, 100]', () => {
-		let p: any = null;
-		let q: any = null;
-		it(`should return true when both trees have 0 node`, () => {
-			expect(solution(p, q)).toBe(true);
+		let tree1: any;
+		let tree2: any;
+		beforeEach(() => {
+			tree1 = null;
+			tree2 = null;
 		})
+		it(`should return true when both trees have 0 node`, () => {
+			expect(solution(tree1, tree2)).toBe(true);
+		});
+
+		it(`should return false when each tree has 0 and 1 node`, () => {
+			tree2 = new TreeNode(2);
+			expect(solution(tree1, tree2)).toBe(false);
+		});
+
+		it(`should return false when each tree has 1 node(and has different value)`, () => {
+			tree1 = new TreeNode(1);
+			expect(solution(tree1, tree2)).toBe(false);
+		});
+
+		// it.each(Array.from({ length: 16 }, (_, index) => index + 1))(
+		// 	`Both trees with %p nodes are the same`, (numNodes) => {
+		// 		tree1 = generateCompleteTree(numNodes);
+		// 		tree2 = generateCompleteTree(numNodes);
+		// 		expect(solution(tree1, tree2)).toBe(true);
+		// 		console.log(tree1.printTreeLevels().join('\n'));
+		// 	}
+		// );
+		it(`print tree test`, () => {
+			tree1 = generateCompleteTree(16);
+			expect(solution(tree1, tree2)).toBe(false);
+			console.log(tree1.printTreeLevels().join('\n'));
+		})
+	});
+
+	// 1~numNodes 값을 가지는 완전 이진 트리 생성 
+	function generateCompleteTree(numNodes: number) {
+		if (numNodes === 0) {
+			return null;
+		}
+
+		const root = new TreeNode(1);
+		const queue = [root];
+		// if (numNodes <= 3) {
+		// 	root.left = new TreeNode(2);
+		// 	if (numNodes > 2) {
+		// 		root.right = new TreeNode(3);
+		// 	}
+		// 	return root;
+		// }
+		let count = 1;
+		while (count < numNodes) {
+			const current = queue.shift()!;
+
+			current.left = new TreeNode(++count);
+			queue.push(current.left);
+
+			if (count < numNodes) {
+				current.right = new TreeNode(++count);
+				queue.push(current.right);
+			}
+		}
+
+		// let current = root.left;
+		// let prev = root;
+		// for (let i = 2; i <= numNodes; i++) {
+		// 	if (i % 2 === 0) {
+		// 		current.left = new TreeNode(i);
+		// 	} else {
+		// 		current.right = new TreeNode(i);
+		// 		if ()
+		// 		current = current.left ? current.left : current;
+		// 	}
+		// }
+
+		return root;
+	}
+	describe('Different trees check: Should return false when...', () => {
+		let tree1: any;
+		let tree2: any;
+		
+		it(`both trees have 2 nodes with different structures: [0,1,null] / [0,null,2]`, () => {
+			tree1 = new TreeNode(0, new TreeNode(1));
+			tree2 = new TreeNode(0, null, new TreeNode(2));
+			expect(solution(tree1, tree2)).toBe(false);
+		});
+
+		it(`bothe tress have 3 nodes with different values: [0,1,3] / [0,1,2]`, () => {
+			tree1.right = new TreeNode(3);
+			tree2.left = new TreeNode(1);
+			expect(solution(tree1, tree2)).toBe(false);
+		});
+	});
+
+	describe('Same trees check: Should return true when...', () => {
+		let tree1: any;
+		let tree2: any;
+
+		it(`both trees have 2 nodes: [0,1,null] / [0,1,null]`, () => {
+			tree1 = new TreeNode(0, new TreeNode(1));
+			tree2 = new TreeNode(0, new TreeNode(1));
+			expect(solution(tree1, tree2)).toBe(true);
+		});
+
+		it(`bothe tress have 3 nodes: [0,1,3] / [0,1,3]`, () => {
+			tree1.right = new TreeNode(3);
+			tree2.right = new TreeNode(3);
+			expect(solution(tree1, tree2)).toBe(true);
+		});
+
+		it(`both trees have 5 nodes: [0,1,3,2,null,4] / [0,1,3,2,null,4]`, () => {
+			tree1.left.left = new TreeNode(2);
+			tree1.right.left = new TreeNode(4);
+			tree2.left.left = new TreeNode(2);
+			tree2.right.left = new TreeNode(4);
+			console.log(tree1.printTreeLevels().join('\n'));
+			expect(solution(tree1, tree2)).toBe(true);
+		});
+
+		it(`sample test to draw trees`, () => {
+			tree1.left.left = new TreeNode(2);
+			tree1.right.left = new TreeNode(4);
+			tree1.right.left.right = new TreeNode(0);
+			tree1.right.left.right.left = new TreeNode(7);
+			tree1.left.left.right = new TreeNode(8, null, new TreeNode(9));
+			console.log(tree1.printTreeLevels().join('\n'));
+		});
 	});
 });
--- leetcode-easy/leet100-똑같은 트리인가.ts
+++ leetcode-easy/leet100-똑같은 트리인가.ts
@@ -1,6 +1,39 @@
-/*
+/* 100. Same Tree
+https://leetcode.com/problems/same-tree/description/
+
+Easy
+
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+ 
+
+Example 1:
+
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+Example 2:
+
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+Example 3:
+
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
 
 */
+
+import { connect } from "http2"
 
 /**
  * Definition for a binary tree node.
@@ -13,8 +46,94 @@ class TreeNode {
         this.val = (val===undefined ? 0 : val)
         this.left = (left===undefined ? null : left)
         this.right = (right===undefined ? null : right)
-    }
+	}
+	
+	printTree(node: TreeNode | null = this, prefix: string = '', isLeft: boolean, result: string[] = []) {
+		if (node === null) {
+			return;
+		}
+
+		const connector = isLeft ? '/' : '\\';
+		console.log(`${prefix}${connector}----${node.val}`);
+
+		const childPrefix = `${prefix}		`;
+		this.printTree(node.left, childPrefix, true);
+		this.printTree(node.right, childPrefix, false);
+	}
+
+	printTreeLevels(node: TreeNode | null = this, isLeftNode: boolean): string[] {
+		const result: string[] = [];
+		if (node === null) {
+			return result;
+		}
+
+		// [각 노드, 노드가 위치한 레벨, 각 노드의 부모 노드]
+		const queue: Array<[TreeNode, number, TreeNode]> = [[node, 0, null]];
+		let currentLevel = 0; 
+		let currentLevelString = '';
+		let connectorLine = '';
+
+		while (queue.length > 0) {	
+			const [currentNode, level, parentNode] = queue.shift()!; // '!': non-null assertion operator. TypeScript에게 해당 값이 항상 존재한다고 알려줌. shift()의 결과가 undefined이더라도 TypeScript의 strict 모드 경고를 발생시키지 않도록 한다.
+
+			// '지금' 레벨에서 출력해야 하는(=같은 레벨의) 노드가 아니면, result 배열에 현재까지 만든 string을 넣고 string을 초기화한다. 
+			if (level !== currentLevel) {
+				result.push(currentLevelString);
+				result.push(connectorLine);
+				currentLevelString = '';
+				connectorLine = '';
+				currentLevel = level;
+			}
+
+			// (낮은 레벨부터 차례로 튀어나올) 노드들이 '지금' (낮은 레벨부터 차례로)그리고 있는 레벨과 같은 동안에는 다음과 같이 한다: 
+			// 1) 자식 노드가 하나라도 있으면 connector를 '/'로 삼고, 없으면 ' '로 삼는다. 
+			let connector = '';
+			if (currentNode.left) {
+				connector += `/`;
+				if (currentNode.right) {
+					connector += '\\ ';
+				} else connector += '  ';
+			} else if (currentNode.right) {
+				connector += ' \\ ';
+			} else connector = '  ';
+			connectorLine += connector;
+
+			// currentLevelString += `${connector}${connector === '/' ? ' ' : '  '}${currentNode.val}`;
+			// if (connector == '/  ') currentLevelString += `${currentNode.val}  `;
+			// else if (connector == '/\\ ') currentLevelString += `${currentNode.val}`;
+			// currentLevelString += `${currentNode.val}  `;
+			if (!parentNode) currentLevelString += `${currentNode.val}`;
+			else if (currentNode && currentNode === parentNode.left) { // 현재 노드가 '왼 자식'
+				currentLevelString += `${currentNode.val} `;
+			} else if (currentNode) { // 현재 노드가 '오른 자식'
+				if (!parentNode.left) {
+					currentLevelString += `  `;
+				}
+				currentLevelString += `${currentNode.val} `;
+			} else { // 현재 노드가 null인 가상의 자식. 근데 적용이 안 되는 것 같다.
+				currentLevelString += '       ';
+			}
+			// 현재 노드에 왼쪽 자식 노드가 있으면 queue(의 끝)에 추가
+			if (currentNode.left) {
+				queue.push([currentNode.left, level + 1, currentNode]);
+			}
+			// 현재 노드에 오른쪽 자식 노드가 있으면 queue에 추가
+			if (currentNode.right) {
+				queue.push([currentNode.right, level + 1, currentNode]);
+			}
+		}
+
+		// 마지막 노드 처리: 현재까지 만들고 있는 string이 빈 문자열 ''이 아니라면 마지막으로 result 배열에 추가해줌. 
+		if (currentLevelString) {
+			result.push(currentLevelString);
+			result.push(connectorLine);
+		}
+
+		return result;
+	}
 }
+
+// => 주어진 두 트리가 같은지를 검사하여 반환하기.
 
 /**
  * @param {TreeNode} p 
@@ -47,7 +166,7 @@ export default {
 	TreeNode,
 }
 
-// 다른 해답: 재귀 풀이
+// 다른 해답: 위의 풀이를 더 축약한 형태
 function solution2(p: TreeNode| null, q: TreeNode| null): boolean {
 	if (!p && !q) return true;
 	if (!p || !q) return false;

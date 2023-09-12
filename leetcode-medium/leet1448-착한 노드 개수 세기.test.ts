b44aa936a7ac49f0f643bfec39167aec645b4fa3
DevOcean Han <soa6318@gmail.com>
2023-09-12 21:03:55 +0900

    [완료][Leet1448] Tree. (Recursive DFS와  stack을 이용한 DFS 해법)

--- leetcode-medium/leet1448-착한 노드 개수 세기.test.ts
+++ leetcode-medium/leet1448-착한 노드 개수 세기.test.ts
@@ -17,5 +17,40 @@ describe('Count Good Nodes in Binary tree', () => {
 			root.left = new TreeNode(0);
 			expect(solution(root)).toBe(2);
 		});
+
+		it(`should return 2 when given tree is [0,null,1]`, () => {
+			root.right = new TreeNode(1);
+			expect(solution(root)).toBe(2);
+		});
+
+		it(`should return 1 when given tree is [0,null,-1]`, () => {
+			root.right = new TreeNode(-1);
+			expect(solution(root)).toBe(1);
+		});
+
+		it(`should return 3 when given tree is [0,0,0]`, () => {
+			root.left = new TreeNode(0);
+			root.right = new TreeNode(0);
+			expect(solution(root)).toBe(3);
+		});
+	});
+
+	describe('Sample cases check:', () => {
+		let root: TreeNode;
+		beforeEach(() => {
+			root = new TreeNode(3);
+		});
+
+		it(`should return 4 when given tree is [3,1,4,3,null,1,5]`, () => {
+			root.left = new TreeNode(1, new TreeNode(3));
+			root.right = new TreeNode(4, new TreeNode(1), new TreeNode(5));
+			expect(solution(root)).toBe(4);
+		});
+
+		it(`should return 3 when given tree is [3,3,null,4,2]`, () => {
+			root.left = new TreeNode(3, new TreeNode(4), new TreeNode(2));
+			expect(solution(root)).toBe(3);
+		});
+
 	});
 });
--- leetcode-medium/leet1448-착한 노드 개수 세기.ts
+++ leetcode-medium/leet1448-착한 노드 개수 세기.ts
@@ -1,4 +1,5 @@
 /* 1448. Count Good Nodes in Binary Tree
+https://leetcode.com/problems/count-good-nodes-in-binary-tree/
 
 Medium
 
@@ -44,7 +45,7 @@ import { TreeNode } from "../Class 모음";
 
 // => 루트로부터 모든 노드에 이르기까지의 길 중 해당 노드가 최대값인 경우(중복 허용)에 해당하는 노드가 몇 개인지 반환하기
 
-function solution(root: TreeNode | null): number {
+function solution1(root: TreeNode | null): number {
 	// 현재 노드 값이 루트로부터 현재 노드까지의 max값 이상이면 '착한'노드로 카운트하여 true를 반환한다. 
 	function aug(node: TreeNode | null, maxVal: number): boolean {
 		if (!node) return true;
@@ -55,10 +56,60 @@ function solution(root: TreeNode | null): number {
 	}
 	let isGood = aug(root, -Infinity);
 
-	return ;
+	// 현재 노드 포함 모든 자식 노드 중 '착한' 노드 개수를 반환한다. 
+	function count(node: TreeNode | null, maxVal: number): number {
+		// 탈출 조건(Base condition): 현재 노드가 null이면 0을 반환한다. (=> 잎 노드는 양쪽 자식으로부터 0과 0을 반환받게 된다)
+		if (!node) return 0;
+
+		// 1) 자기 자신 노드가 '착한'노드인지 여부(+1 혹은 +0) 더하기
+		// 2) 왼쪽 자식 트리의 '착한'노드 누적 개수 더하기
+		// 3) 오른쪽 자식 트리의 '착한'노드 누적 개수를 더하여 반환한다.
+		return +(node.val >= maxVal) +
+		// return (node.val >= maxVal ? 1 : 0) +
+			count(node.left, Math.max(maxVal, node.val)) +
+			count(node.right, Math.max(maxVal, node.val));
+	}
+
+	return count(root, root.val);
+}
+
+// solution1을 리팩토링한 버전: 
+function solution2(root: TreeNode | null): number {
+	function count(node: TreeNode | null, maxVal: number): number {
+		if (!node) return 0;
+
+		const isGoodNode = node.val >= maxVal;
+		const leftGoodNodes = count(node.left, isGoodNode ? node.val : maxVal);
+		const rightGoodNodes = count(node.right, isGoodNode ? node.val : maxVal);
+
+		return +isGoodNode + leftGoodNodes + rightGoodNodes;
+	}
+
+	return count(root, -Infinity);
 }
 
 export default {
-	solution: solution,
+	solution: solution3,
 }
 
+// 다른 해답: 재귀(recursive) 말고 스택을 이용하여 반복(iterative)함. 
+function solution3(root: TreeNode | null): number {
+
+	const stack: Array<[TreeNode, number]> = [[root, root.val]];
+	let count:number = 0;
+
+	while (stack.length) {
+		const [node, currentMax] = stack.pop();
+		count += (node.val >= currentMax ? 1 : 0);
+
+		for (let child of [node.left, node.right]) {
+			if (child) {
+				stack.push([child, Math.max(node.val, currentMax)]);
+			}
+		}
+		// if (node.left) stack.push([node.left, Math.max(node.val, currentMax)]);
+		// if (node.right) stack.push([node.right, Math.max(node.val, currentMax)]);
+	}
+
+	return count;
+}

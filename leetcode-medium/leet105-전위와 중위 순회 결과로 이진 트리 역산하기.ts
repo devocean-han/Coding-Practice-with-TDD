252347914be44bff7be4b9bf52fbc95fd2ac0f55
DevOcean Han <soa6318@gmail.com>
2023-09-29 22:17:47 +0900

    [진행중][Leet105] Binary Tree

--- leetcode-medium/leet105-전위와 중위 순회 결과로 이진 트리 역산하기.ts
+++ leetcode-medium/leet105-전위와 중위 순회 결과로 이진 트리 역산하기.ts
@@ -68,6 +68,20 @@ function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
 		// 예를 들어 inorder의 첫 값이 preorder 첫 값, 즉 root와 같다면 root에게 왼 자식이 없음을 알 수 있다. 
 		if (tree.val === inorder[0]) tree.right = node, tree = tree.right;
 		else tree.left = node, tree = tree.left;
+
+		/* 
+		일단 같은 자리에 [3,9], [9,3]이렇게 순서만 바뀐 노드가 오는 때는 반드시 [3,9,null]의 부모-왼자식 관계가 확정된다.
+		노드를 2개씩 짝지어 서브 트리를 그려 가능한 경우의 수를 짝지어 전체 트리를 그려봤지만 일관되게 설명하기엔 복잡하다.
+		일단 preorder 배열의 연이은 두 값이 만들 수 있는 관계는 뒤의 값을 기준으로:
+		1. 왼쪽 자식이 된다
+		2. 외동 오른 자식이 된다
+		3. 왼 형제가 있는 오른 형제가 된다
+		의 세 가지가 가능하고, inorder 배열의 연이은 두 값은 역시나 뒤의 값을 기준으로: 
+		1. 오른 자식이 된다
+		2. 왼 자식의 부모가 된다
+		3. 오른 자식의 n대 왼 자손이 된다
+		이렇게 세 가지가 가능하다.
+		*/
 	}
     
 	return tree;

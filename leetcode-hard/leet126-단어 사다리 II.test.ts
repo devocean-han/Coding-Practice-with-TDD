97c5275bb017db93254c4ff20517e9b6c484fc93
DevOcean Han <soa6318@gmail.com>
2024-01-05 21:34:49 +0900

    [완료][Leet126] - 2. (실패) - 3. (성공) 간선을 통과한 개수인 '레벨'마다 가능한 단어를 몽땅 모아두는 풀이로 성공 - 4. (진행중) 타입스크립트 문법을 대거 적용한 풀이

--- leetcode-hard/leet126-단어 사다리 II.test.ts
+++ leetcode-hard/leet126-단어 사다리 II.test.ts
@@ -27,12 +27,12 @@ describe('Word Ladder II', () => {
 			expect(solution(beginWord, endWord, wordList)).toEqual([["red","ted","tad","tax"],["red","ted","tex","tax"],["red","rex","tex","tax"]]);
 		});
 	});
-	// describe('Error case 19: "qa"->"sq", wordList=["si","go","se", ...]', () => {
-	// 	let beginWord: string = "qa";
-	// 	let endWord: string = "sq";
-	// 	let wordList: string[] = ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"];
-	// 	it(`should return 3 transformation sequences:\n\t"red" -> "ted" -> "tad" -> "tax"\n\t"red" -> "ted" -> "tex" -> "tax"\n\t"red" -> "rex" -> "tex" -> "tax"`, () => {
-	// 		expect(solution(beginWord, endWord, wordList)).toEqual([["red","ted","tad","tax"],["red","ted","tex","tax"],["red","rex","tex","tax"]]);
-	// 	});
-	// });
+	describe('Error case 19: "qa"->"sq", wordList=["si","go","se", ...]', () => {
+		let beginWord: string = "qa";
+		let endWord: string = "sq";
+		let wordList: string[] = ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"];
+		it(`should return 3 transformation sequences:\n\t"red" -> "ted" -> "tad" -> "tax"\n\t"red" -> "ted" -> "tex" -> "tax"\n\t"red" -> "rex" -> "tex" -> "tax"`, () => {
+			expect(solution(beginWord, endWord, wordList)).toEqual([["red","ted","tad","tax"],["red","ted","tex","tax"],["red","rex","tex","tax"]]);
+		});
+	});
 });
--- leetcode-hard/leet126-단어 사다리 II.ts
+++ leetcode-hard/leet126-단어 사다리 II.ts
@@ -130,7 +130,7 @@ function findLadders1(beginWord: string, endWord: string, wordList: string[]): s
 };
 
 
-// 위의 풀이를 시간이 덜 걸리게 리팩토링: 
+// (샘플 케이스에서 실패)위의 풀이를 시간이 덜 걸리게 리팩토링: 
 function findLadders2(beginWord: string, endWord: string, wordList: string[]): string[][] {
 	const wordSet = new Set<String>(wordList);	
 	// endWord가 wordList에 들어있지 않으면 endWord에 도달할 수 없음
@@ -216,6 +216,201 @@ function findLadders2(beginWord: string, endWord: string, wordList: string[]): s
 };
 
 
+// (성공) 단계별로 가능한 모든 단어를 저장하고, 나중에 끝 단어부터 루트를 꿰어 맞춰 반환하는 풀이: 
+function findLadders3(beginWord: string, endWord: string, wordList: string[]): string[][] {
+	let wordSet = new Set(wordList);
+	// endWord가 wordList에 들어있지 않으면 endWord에 도달할 수 없음
+	if (!wordSet.has(endWord))
+		return [];
+
+	// 두 단어 word1과 word2를 받아 서로 한 글자만 다르면 '연결된 노드'로 판단하고 true를 반환하는 함수
+	function connected(word1: string, word2: string): boolean {
+		let differentChars = 0;
+		for (let i = 0; i < word1.length && differentChars < 2; i++) {
+			if (word1[i] !== word2[i])
+				differentChars++;
+		}
+		return differentChars === 1;
+	}
+
+	// 초기화: 큐에 단어들이 묶음별로 취급되고 소비되는 것이 핵심
+	wordSet.delete(beginWord)
+	let queue: string[] = [beginWord]; // '한 글자가 다른' 이웃 단어를 발견할 때마다 저장하고, 각 이웃의 이웃들을 탐색할 때마다 하나씩 뺄 단어 저장 공간
+	let validWordsPerLevel = []; // 진행할 수 있는 레벨 별 단어 묶음. [[0레벨의 단어들], [1레벨의 단어들], ... ,]
+	let reached = false; // 탐색중 목표 단어를 만났는지 여부
+
+	// BFS: 같은 진행 단계(level)의 모든 진행할 수 있는 단어를 순회하며 진행 할 수 있는 이웃 단어를 전부 찾아 queue에 한 묶음으로 넣음. 
+	// Time complexity: 단계(레벨) 수 * 각 단계별 유효 단어 수 * 전체 단어 중 미방문 단어 수 = O(N*3)..?
+	while (queue.length && !reached) {
+		validWordsPerLevel.push([...queue]); // 현 시점 queue: 이 전 단계에서 쌓인 '진행 가능한' 모든 이웃 단어 목록
+		/*
+		Q: [hit\   hot\  dot\  lot\  dog\  log] 
+		L: [[hit], [hot], [dot,lot], [dog,log]]
+		qlen: 1		1		2			2
+		from: hit   hot   dot  lot	 dog
+		*/
+		let qlen = queue.length; // 현재 큐 길이를 고정하여 순회
+		for (let i = 0; i < qlen && !reached; i++) {
+			let from = queue.shift(); 
+			for (let to of wordSet) {
+				// 두 단어가 연결된 노드가 아니면 다음 단어 탐색
+				if (!connected(from, to))
+					continue;
+				// 지금 단어가 목표 단어라면 탐색 중단
+				if (to === endWord) {
+					reached = true;
+					break;
+				}
+				// 두 단어가 연결된 노드일 때만
+				// - 지금 노드에 연결된 또 다른 노드를 살피러 큐에 넣고
+				// - 이후의 다른 노드들이 검사할 '연결될 수 있는 대상' 후보에서 뺌 (중복 방문 방지)
+				queue.push(to);
+				wordSet.delete(to);
+				// => 다른 레벨에서 같은 단어를 이웃으로 가질 수도 있는데 이렇게 '어떤 단어의 이웃임을 발견하자마자' '미방문' 목록에서 삭제할 수 있는 이유는, 어차피 '가장 짧은 길'을 찾는 문제라 그러함. 이렇게 하면 '돌고 돌아 나중에 다시 만날 수도 있는' 루트는 아예 폐쇄하게 됨. 
+			}
+		}
+	}
+	/*
+		Q: [hit\   hot\  dot\  lot\  dog\  log] 
+		L: [[hit], [hot], [dot,lot], [dog,log]]
+		qlen: 1		1		2			2
+		from: hit   hot   dot  lot	 dog
+
+		ans: [[cog]\ [dog,cog]\,[log,cog]\,  [dot,dog,cog]\,[lot,log,cog]\]
+				[hot,dot,dog,cog]\,[hot,lot,log,cog]\,
+				[hit,hot,dot,dog,cog], [hit,hot,lot,log,cog]]
+		len:  1			2
+		P:    [cog]  [dog,cog]  [log,cog]
+		lWord: cog		dog			log
+	*/
+	if (!reached) return [];
+
+	// 마지막 단어부터 가능한 단어를 거꾸로 추가해 나가며 정답 루트 목록을 완성함
+	let ans = [[endWord]];
+	for (let level = validWordsPerLevel.length - 1; level >= 0; level--) {
+		let len = ans.length; // 현재의 ans의 길이를 고정하여 순회
+		for (let i = 0; i < len; i++) { //^ 여기서 그냥 ans.length로 지정하는 것과 결과가 다르다
+			let currentPath = ans.shift();
+			let lastWord = currentPath[0];
+			for (let word of validWordsPerLevel[level]) {
+				if (!connected(lastWord, word))
+					continue;
+				ans.push([word, ...currentPath]);
+			}
+		}
+	}
+
+	return ans;
+}
+
+// 또다른 해답: Typescript 문법을 좀 더 이용하는
+function findLadders4(beginWord: string, endWord: string, wordList: string[]): string[][] {
+	interface NodeInterface {
+		name: string;
+		path: NodeInterface[];
+	}
+
+	const encodeHash = new Map<string, Set<string>>();
+	const wordsTemplates: { [key: string]: string[] } = {};
+	const tree: { [key: string]: Set<string> } = {};
+	let resultNodes: NodeInterface | null = null;
+
+	for (const word of [beginWord, ...wordList]) {
+		wordsTemplates[word] = [];
+		for (let i = 0; i < word.length; i++) {
+			const encode = word.slice(0, i) + '*' + word.slice(i + 1);
+
+			// Avoid jump from end word to itself
+			if (word !== endWord) {
+				wordsTemplates[word].push(encode);
+			}
+			if (encodeHash.has(encode)) {
+				const set = encodeHash.get(encode);
+				set?.add(word);
+			} else {
+				const set = new Set<string>();
+				set.add(word);
+				encodeHash.set(encode, set);
+			}
+		}
+	}
+
+	for (const key in wordsTemplates) {
+		for (const template of wordsTemplates[key]) {
+			const items = encodeHash.get(template);
+
+			if (!tree[key]) {
+				tree[key] = new Set();
+			}
+			for (const item of items ?? []) {
+				tree[key].add(item);
+			}
+		}
+		tree[key]?.delete(key);
+	}
+	tree[endWord] = new Set();
+
+	let queue: NodeInterface[] = [{ name: beginWord, path: [] }];
+	const newQueue = new Map<string, NodeInterface>();
+	let excludedNodes = new Set<string>();
+
+	while (queue.length) {
+
+		for (const nodeFromQueue of queue) {
+
+			for (const word of tree[nodeFromQueue.name]) {
+				if (excludedNodes.has(word)) {
+					continue;
+				}
+
+				const futureNode = newQueue.get(word);
+				if (futureNode) {
+					futureNode.path.push(nodeFromQueue);
+				} else {
+					newQueue.set(word, { name: word, path: [nodeFromQueue] });
+				}
+			}
+
+			excludedNodes.add(nodeFromQueue.name);
+		}
+
+		const endNode = newQueue.get(endWord);
+		if (endNode) {
+			if (resultNodes) {
+				endNode.path.forEach(e => (resultNodes?.path?.push(e)));
+			} else {
+				resultNodes = endNode;
+			}
+			break;
+		}
+
+		queue = Array.from(newQueue.values()).filter(e => !excludedNodes.has(e.name));
+		newQueue.clear();
+	}
+
+	const dfs = (root: NodeInterface): string[][] => {
+		let result: string[][] = [];
+
+		if (root.path.length === 0) {
+			return [[root.name]];
+		}
+
+		for (const node of root.path) {
+			dfs(node).forEach(e => (result.push([...e, root.name])));
+		}
+
+		return result;
+	};
+
+	return resultNodes ? dfs(resultNodes) : [];
+}
+		
+/*
+	^해답 1,2,3에서 관계를 매핑하는 방법의 차이: 
+	방법1: wordList에 존재하는 모든 두 단어 쌍을 검사하여(N^2) 매핑 객체로 만듬
+	방법2: wordList 각각의 단어들로 만들 수 있는 모든 '한 문자 변경 단어'들이 wordList에 존재하는지를 검사(N)하여 매핑 객체로 만듬
+	방법3: 대상 노드를 정해두고 wordList의 미방문 단어들을 하나씩 대어보며 검사 
+ */
 export default {
-	solution: findLadders2,
+	solution: findLadders4,
 }
